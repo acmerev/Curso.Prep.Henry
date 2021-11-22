e4ed88fb9a4d84e0959341545fdd24862105fb30
Emmanuel Montiel Cruz <acmerev@gmail.com>
2021-11-22 17:04:26 -0600

    commit on 'master'

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -77,39 +77,43 @@ const divide = (x, y) => x/y;
   //     return false;     
 //} 
 
-const sonIguales = (x, y) => { if (x == y){ return true;} return false;}
+const sonIguales = (x, y) =>  x === y;
 
- 
 
-function tienenMismaLongitud(str1, str2) {
+//function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length == str2.length ){
-    return true
-  }
-  return false
-}
+ // if (str1.length == str2.length ){
+//    return true
+ // }
+//  return false
+//}
 
-function menosQueNoventa(num) {
+const tienenMismaLongitud = (str1, str2) => str1.length === str2.length;
+
+//function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num<90){
-      return true
-  }
-  return false
-}
+  //if (num<90){
+    //  return true
+  //}
+  //return false
+//}
 
-function mayorQueCincuenta(num) {
+const menosQueNoventa = num => num < 90;
+
+//function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num>50) {
-    return true
-  }
-  return false
-}
+ // if (num>50) {
+ //   return true
+//  }
+//  return false
+//}
+const mayorQueCincuenta = num => num >50;
 
 //function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
@@ -118,17 +122,18 @@ function mayorQueCincuenta(num) {
 //}
 const obtenerResto = (x, y) => x%y;
 
-function esPar(num) {
+//function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num%2 == 0)
-  {
-return true
+ // if (num%2 == 0)
+ // {
+//return true
 
-  }
-  return false
-}
+ // }
+ // return false
+// }
+const esPar = num => num%2 ==0; 
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
@@ -218,19 +223,23 @@ const esPositivo = numero => {
 const agregarSimboloExclamacion = str => str + '!';
 
 
-function combinarNombres(nombre, apellido) {
+//function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return nombre + ' ' + apellido;
-}
+  //return nombre + ' ' + apellido;
+//}
 
-function obtenerSaludo(nombre) {
+const combinarNombres = (nombre, apellido) => nombre + ' ' + apellido;
+
+//function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' +' '+ nombre + '!';
-}
+  //return 'Hola' +' '+ nombre + '!';
+//}
+
+const obtenerSaludo = nombre => 'Hola' +' '+ nombre + '!';
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
